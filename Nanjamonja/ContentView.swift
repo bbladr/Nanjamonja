c5387550a3df78a8eb1c918226631c34ff43eabb
yusuketk <yusuketk@weseek.co.jp>
2020-07-19 00:28:23 +0900

    Initial Commit

---
+++ Nanjamonja/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nanjamonja
+//
+//  Created by Yusuke Takizawa on 2020/07/19.
+//  Copyright © 2020 Yusuke Takizawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
